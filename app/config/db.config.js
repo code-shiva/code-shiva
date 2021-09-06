5c8b8cdd22626038e660e84482c99234682c04a8
code-shiva <90215108+code-shiva@users.noreply.github.com>
2021-09-07 00:29:46 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,5 @@
 module.exports ={
-  //HOST: 'ec2-52-22-161-59.compute-1.amazonaws.com',
-  HOST:'postgres://heceuejmfwcuhi:204ad16b44a356cbbc030961192d4982fb9b50956c4b02f2f7eeb5a47f66d3bf@ec2-52-22-161-59.compute-1.amazonaws.com:5432/d4a3n2sq0q91ii',
+  HOST: 'ec2-52-22-161-59.compute-1.amazonaws.com',
   USER: 'heceuejmfwcuhi',
   PASSWORD: '204ad16b44a356cbbc030961192d4982fb9b50956c4b02f2f7eeb5a47f66d3bf',
   DB: 'd4a3n2sq0q91ii',
